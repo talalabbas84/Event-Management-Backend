e644e9bed4b3b5cb4d943ad03ca9a9ce7be4d358
Syed Talal Abbas Jafri <syedtalal.abbas@systemsltd.com>
2021-08-07 04:09:08 +0500

    FSDDFS

--- controllers/venue.js
+++ controllers/venue.js
@@ -46,7 +46,7 @@ exports.addVenue = asynchandler(async (req, res, next) => {
       venue.images = url;
       venue.user = req.user._id;
       console.log(venue);
-      console.log({ ...venue });
+      console.log(...JSON.parse(venue));
       const venueData = await Venue.create({ venue });
       return res.status(200).json({
         success: true,
