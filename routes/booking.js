8ba88623de4f555e532e9c4e4355a8c5faa84c5c
Syed Talal Abbas Jafri <syedtalal.abbas@systemsltd.com>
2021-08-11 01:13:46 +0500

    FSDSDF

--- routes/booking.js
+++ routes/booking.js
@@ -15,23 +15,11 @@ router
   // .get(getVenues)
   .post(protect, createBooking);
 
-router.route("/accept/:id").put(acceptBooking);
+router.route("/accept").put(acceptBooking);
 
 router.route("/delete/:id").delete(deleteBooking);
 
 router.route("/vendor/:id").get(protect, getAllBookingsByVendor);
 router.route("/venue/:id").get(getAllBookingsByVenue);
-
-// router.route("/vendor/:id").get(getVenuesByVendorID);
-// router
-//   .route("/image/:id")
-//   .post(addImageToVenue)
-//   .put(RemoveImageFromVenue);
-
-// router
-//   .route("/:id")
-//   .get(getVenue)
-//   .put(protect, authorize("vendor"), updateVenue)
-//   .delete(protect, authorize("vendor"), deleteVenue);
 
 module.exports = router;
